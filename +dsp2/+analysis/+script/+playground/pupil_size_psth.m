5bf852d3739b19d4be7184a08c3f121dfa109e4d
nfagan <nick@fagan.org>
2017-08-21 17:51:38 -0400

    separated process and plot

--- +dsp2/+analysis/+script/+playground/pupil_size_psth.m
+++ +dsp2/+analysis/+script/+playground/pupil_size_psth.m
@@ -1,15 +1,18 @@
 %%  load
 import dsp2.process.format.*;
 
+dsp2.cluster.init();
+conf = dsp2.config.load();
+save_path = fullfile( conf.PATHS.analyses, 'pupil' );
+dsp2.util.general.require_dir( save_path );
+fname = 'n_minus_one_size.mat';
+tfname = 'time_series.mat';
+
 [events, key] = dsp2.io.get_events();
 gaze = dsp2.io.get_gaze_data();
 gaze = gaze.only( {'px', 'py', 'pt'} );
 
 %%  get psth
-
-days = gaze( 'days' );
-days = days( randperm(numel(days), 10) );
-gaze = gaze.only( days );
 
 look_back = -.15;
 look_amt = .6;
@@ -27,9 +30,8 @@ errs = errs | event.data(:, 1) < 0;
 event.data( errs, : ) = 0;
 
 gz = gaze.only( {'pt', 'px', 'py'} );
-evt = event.only( gz('days') );
 
-psth = dsp2.process.format.get_gaze_psth( evt, gz, 'pt' );
+psth = get_gaze_psth( evt, gz, 'pt' );
 errs = isnan( psth.data(:, 1) );
 %%  get n minus 1
 
@@ -45,48 +47,8 @@ nlabel = sprintf( 'n_minus_%d', nprev );
 nmn = nmn.for_each( 'gaze_data_type', @add_trial_ids );
 nmn = nmn.for_each( 'gaze_data_type', @get_n_minus_n_distribution, nprev );
 
-prev = nmn.only( nlabel );
-curr = nmn.only( 'n_minus_0' );
-prev( 'outcomes' ) = curr( 'outcomes', : );
+%%  save
 
-%%  normalize
-
-norm_ind = (x >= look_back & x <= 0);
-extr = prev;
-extr.data = extr.data(:, norm_ind);
-meaned = mean( extr.data, 2 );
-
-for i = 1:size(prev.data, 2)
-  prev.data(:, i) = prev.data(:, i) ./ meaned;
-end
-
-%%  plot
-
-plt = prev.only( 'px' );
-
-plt = plt.rm( 'errors' );
-plt.data = abs( plt.data );
-
-plt1 = plt;
-plt1 = plt1.replace( {'self', 'none'}, 'antisocial' );
-plt1 = plt1.replace( {'both', 'other'}, 'prosocial' );
-plt1 = plt1.add_field( 'group_type', 'pro_v_anti' );
-
-plt2 = plt;
-plt2 = plt2.add_field( 'group_type', 'per_outcome' );
-
-plt = plt1.append( plt2 );
-
-plt = plt.parfor_each( {'outcomes', 'days'}, @mean );
-
-figure(1); clf();
-
-pl = ContainerPlotter();
-pl.add_ribbon = true;
-pl.x = x;
-pl.vertical_lines_at = 0;
-pl.y_label = 'Pupil size';
-pl.x_label = 'Time (ms) from mag cue onset';
-
-plt.plot( pl, {'outcomes'}, 'group_type' );
+save( fullfile(save_path, fname), 'nmn' );
+save( fullfile(save_path, tfname), 'x' );
 
---
+++ +dsp2/+plot/+script/+playground/pupil_psth.m
@@ -0,0 +1,51 @@
+%%  plot
+
+conf = dsp2.config.load();
+pathstr = fullfile( conf.PATHS.analyses, 'pupil' );
+nmn = dsp2.util.general.fload( fullfile(pathstr, 'n_minus_one_size.mat') );
+x = dsp2.util.general.fload( fullfile(pathstr, 'time_series.mat') );
+
+%%  normalize
+
+prev = nmn.only( 'n_minus_1' );
+curr = nmn.only( 'n_minus_0' );
+prev( 'outcomes' ) = curr( 'outcomes', : );
+
+norm_ind = (x >= look_back & x <= 0);
+extr = prev;
+extr.data = extr.data(:, norm_ind);
+meaned = mean( extr.data, 2 );
+
+for i = 1:size(prev.data, 2)
+  prev.data(:, i) = prev.data(:, i) ./ meaned;
+end
+
+%%  plot
+
+plt = prev.only( 'px' );
+
+plt = plt.rm( 'errors' );
+plt.data = abs( plt.data );
+
+plt1 = plt;
+plt1 = plt1.replace( {'self', 'none'}, 'antisocial' );
+plt1 = plt1.replace( {'both', 'other'}, 'prosocial' );
+plt1 = plt1.add_field( 'group_type', 'pro_v_anti' );
+
+plt2 = plt;
+plt2 = plt2.add_field( 'group_type', 'per_outcome' );
+
+plt = plt1.append( plt2 );
+
+plt = plt.parfor_each( {'outcomes', 'days'}, @mean );
+
+figure(1); clf();
+
+pl = ContainerPlotter();
+pl.add_ribbon = true;
+pl.x = x;
+pl.vertical_lines_at = 0;
+pl.y_label = 'Pupil size';
+pl.x_label = 'Time (ms) from mag cue onset';
+
+plt.plot( pl, {'outcomes'}, 'group_type' );
