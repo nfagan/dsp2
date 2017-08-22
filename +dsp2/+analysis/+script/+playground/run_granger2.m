72cbb173a53cd9c101fde204883e7fbf52560aea
nfagan <nick@fagan.org>
2017-08-22 11:52:48 -0400

    made script more flexible

--- +dsp2/+analysis/+script/+playground/run_granger2.m
+++ +dsp2/+analysis/+script/+playground/run_granger2.m
@@ -8,37 +8,49 @@ conf = dsp2.config.load();
 run( fullfile(conf.PATHS.repositories, 'mvgc_v1.0', 'startup.m') );
 %   get signals
 io = dsp2.io.get_dsp_h5();
-epoch = 'targacq';
+epoch = 'reward';
+tmp_fname = [ epoch, '.txt' ];
 P = io.fullfile( 'Signals/none/complete', epoch );
 signals = io.read( P );
 %   set up save paths
 save_path = fullfile( conf.PATHS.analyses, 'granger', epoch );
 dsp2.util.general.require_dir( save_path );
-conf.PATHS.dynamic.granger = save_path;
-dsp2.config.save( conf );
 
 %%  preprocess signals
-tmp_write( '-clear' );
-tmp_write( 'Preprocessing signals ... ' );
+tmp_write( '-clear', tmp_fname );
+tmp_write( 'Preprocessing signals ... ', tmp_fname );
 
-signals_ = signals.rm( 'cued' );
+if ( strcmp(epoch, 'targacq') )
+  signals_ = signals.rm( 'cued' );
+end
 
 signals_ = update_min( update_max(signals_) );
 signals_ = dsp2.process.reference.reference_subtract_within_day( signals_ );
 signals_ = signals_.filter();
 signals_ = signals_.rm( 'errors' );
-signals_.data = signals_.data(:, 301:500 );
-signals_ = signals_.parfor_each( {'channels', 'days'}, @dsp2.process.reference.detrend );
 
-tmp_write( 'Done\n' );
+if ( strcmp(epoch, 'targacq') )
+  % [ -200, 0 ]
+  signals_.data = signals_.data(:, 301:500 );
+elseif ( strcmp(epoch, 'reward') )
+  % [ 50, 250 ]
+  signals_.data = signals_.data(:, 1051:(1050+200));
+else
+  error( 'Script not defined for ''%s''.', epoch );
+end
+
+signals_ = signals_.parfor_each( {'channels', 'days'} ...
+  , @dsp2.process.reference.detrend );
+
+tmp_write( 'Done\n', tmp_fname );
 
 %%  run analysis
 
 days = signals_( 'days' );
 
-for i = 55:numel(days)
+for i = 1:numel(days)
 
-tmp_write( {'Processing %s (%d of %d)\n', days{i}, i, numel(days)} );
+tmp_write( {'Processing %s (%d of %d)\n', days{i}, i, numel(days)}, tmp_fname );
 
 signals2 = signals_.only( days{i} );
 G = signals2.for_each( {'outcomes', 'days'} ...
