3eb63d1597178018097ab3d1f2912d57b4551d6f
nfagan <nick@fagan.org>
2017-08-23 10:47:47 -0400

    added convert_granger script

---
+++ +dsp2/+cluster/+script/convert_granger.m
@@ -0,0 +1,24 @@
+%%  convert granger
+
+dsp2.cluster.init();
+conf = dsp2.config.load();
+load_path = fullfile( conf.PATHS.analyses, 'granger' );
+
+epochs = dsp2.util.general.dirnames( load_path, 'folders' );
+
+for i = 1:numel(epochs)  
+  fload_path = fullfile( load_path, epochs{i} );
+  fsave_path = fullfile( fload_path, 'converted' );
+  
+  dsp2.util.general.require_dir( fsave_path );
+
+  G = dsp2.util.general.load_mats( load_path );
+  G = extend( G{:} );
+
+  G2 = G.parfor_each( {'outcomes', 'trialtypes', 'days', 'channels'} ...
+    , @dsp2.analysis.playground.convert_granger );
+  
+  save( fullfile(fsave_path, 'converted.mat'), 'G2' );
+end
+
+exit();
--- +dsp2/+plot/+script/+playground/plot_granger.m
+++ +dsp2/+plot/+script/+playground/plot_granger.m
@@ -2,12 +2,15 @@
 
 dsp2.cluster.init();
 conf = dsp2.config.load();
-dsp2.util.assertions.assert__dynamic_path_defined( 'granger', conf );
-load_path = conf.PATHS.dynamic.granger;
-G = dsp2.util.general.load_mats( load_path );
+epoch = 'reward';
+load_path = fullfile( conf.PATHS.analyses, 'granger', epoch );
+G = dsp2.util.general.load_mats( load_path, true );
+G = extend( G{:} );
 
-G2 = G.parfor_each( {'outcomes', 'days', 'channels'}, @dsp2.analysis.playground.convert_granger );
+G2 = G.parfor_each( {'outcomes', 'trialtypes', 'days', 'channels'} ...
+  , @dsp2.analysis.playground.convert_granger );
 
+%%
 % G3 = dsp2.process.manipulations.pro_v_anti( G2 );
 
 % g_days = G2( 'days' );
@@ -17,12 +20,12 @@ G3 = G2;
 
 % G3 = G3.rm( 'null_distribution' );
 
-cts = G2.counts('days');
-many_days = unique( cts('days', cts.data == 4096) );
-others = G3.only( many_days );
-others = others.for_each( {'outcomes', 'days', 'regions'}, @subsample, 'channels', 16 );
-G3 = G3.rm( many_days );
-G3 = G3.append( others );
+% cts = G2.counts('days');
+% many_days = unique( cts('days', cts.data == 4096) );
+% others = G3.only( many_days );
+% others = others.for_each( {'outcomes', 'days', 'regions'}, @subsample, 'channels', 16 );
+% G3 = G3.rm( many_days );
+% G3 = G3.append( others );
 
 G3.plot( 'kind', {'outcomes', 'regions'} ...
   , 'shape', [4, 2] ...
