aec34d60d3496ce170b6fe35d7759c150965eb65
nfagan <nick@fagan.org>
2017-09-13 15:14:55 -0400

    changed epochs

--- +dsp2/+plot/+script/null_granger.m
+++ +dsp2/+plot/+script/null_granger.m
@@ -5,10 +5,16 @@ import dsp2.util.general.flatten;
 import dsp2.util.general.load_mats;
 
 m_within = { 'outcomes', 'trialtypes', 'regions', 'permuted', 'channels' ...
-  , 'epochs', 'days' };
+  , 'epochs', 'days', 'administration' };
 
+COLLAPSE_DRUGS = false;
+if ( COLLAPSE_DRUGS )
+  subdir = 'null';
+else
+  subdir = 'drug_effect_null';
+end
 conf = dsp2.config.load();
-load_p = fullfile( conf.PATHS.analyses, 'granger', 'null' );
+load_p = fullfile( conf.PATHS.analyses, 'granger', subdir );
 % epochs = dsp2.util.general.dirnames( load_p, 'folders' );
 epochs = { 'targacq' };
 per_epoch = cell( 1, numel(epochs) );
@@ -23,6 +29,12 @@ for i = 1:numel( epochs )
     fprintf( '\n\t - Processing %s (%d of %d)', mats{k}, k, numel(mats) );
     current = dsp2.util.general.fload( fullfile(fullp, mats{k}) );
     current.data = real( current.data );
+    %
+    %   get rid of drug / administration
+    %
+    if ( COLLAPSE_DRUGS )
+      current = current.collapse( {'drugs', 'administration'} );
+    end
     current = current.for_each_1d( m_within, @Container.nanmean_1d );
     loaded{k} = current;
   end
@@ -33,44 +45,9 @@ per_epoch = flatten( per_epoch );
 
 per_epoch = per_epoch.add_field( 'max_lags', '5e3' );
 
-%%  MAKE PRO V ANTI
+%%  keep within n deviations
 
 proanti = per_epoch;
-% proanti = dsp2.process.manipulations.pro_v_anti( proanti );
-
-%%  PLOT
-
-scale_name = 'rescaled';
-base_fname = sprintf( '%s_%s', strjoin(meaned('epochs'), '_'), scale_name );
-
-meaned = dsp2.process.manipulations.pro_v_anti( kept2.keep_within_freqs( [0, 100] ) );
-% meaned = meaned.only( {'targOn', 'cued'} );
-% meaned = meaned.except( {'targOn', 'choice'} );
-% meaned = meaned.replace( {'targOn', 'targAcq'}, 'choice+cue' );
-% meaned = meaned.rm( 'permuted__true' );
-
-pl = ContainerPlotter();
-pl.compare_series = false;
-pl.marker_size = 2;
-pl.add_ribbon = true;
-pl.main_line_width = 1;
-pl.x = meaned.frequencies;
-pl.shape = [2, 2];
-pl.y_lim = [-.02, .02];
-pl.y_label = 'Granger difference';
-pl.x_label = 'hz';
-pl.order_by = { 'real', 'permuted' };
-
-figure(1); clf();
-
-meaned.plot( pl, {'permuted', 'trialtypes', 'max_lags'}, {'regions', 'outcomes', 'epochs'} );
-
-save_path = fullfile( conf.PATHS.plots, dsp2.process.format.get_date_dir(), 'granger' );
-dsp2.util.general.require_dir( save_path );
-fname = fullfile( save_path, base_fname );
-dsp2.util.general.save_fig( figure(1), fname, {'fig', 'png', 'epsc'} );
-
-%%  keep within n deviations
 
 ndevs = 1.5;
 
@@ -87,7 +64,7 @@ end
 
 mean_func = @Container.nanmean_1d;
 std_func = @Container.nanstd_1d;
-band_mean_within = { 'band', 'outcomes', 'trialtypes' };
+band_mean_within = { 'band', 'outcomes', 'trialtypes', 'drugs', 'administration' };
 
 means = band_means.for_each_1d( band_mean_within, mean_func );
 devs = band_means.for_each_1d( band_mean_within, std_func );
@@ -99,7 +76,7 @@ down_thresh = means - devs;
 
 to_keep = band_means.logic( false );
 
-within = { 'band', 'outcomes', 'trialtypes' };
+within = { 'band', 'outcomes', 'trialtypes', 'drugs', 'administration' };
 cmbs = band_means.pcombs( within );
 
 band_means_data = band_means.data;
@@ -123,7 +100,7 @@ end
 
 kept = proanti.keep( all_keep );
 
-check_sites_within = { 'outcomes', 'trialtypes', 'days', 'channels', 'regions' };
+check_sites_within = { 'outcomes', 'trialtypes', 'days', 'channels', 'regions', 'administration' };
 
 kept_cmbs = kept.pcombs( check_sites_within );
 orig_cmbs = proanti.pcombs( check_sites_within );
@@ -134,9 +111,52 @@ orig_cmbs = dsp2.util.general.array_join( orig_cmbs );
 missing = setdiff( orig_cmbs, kept_cmbs );
 
 require_per = { 'days', 'channels', 'regions', 'permuted' };
-required_labs = proanti.pcombs( {'outcomes', 'trialtypes'} );
+required_labs = proanti.pcombs( {'outcomes', 'trialtypes', 'administration'} );
 
 kept2 = dsp2.util.general.require_labels( kept, require_per, required_labs );
+
+%%  MAKE PRO V ANTI
+
+proanti = kept2.keep_within_freqs( [0, 100] );
+% proanti = per_epoch.keep_within_freqs( [0, 100] );
+proanti = dsp2.process.manipulations.pro_v_anti( proanti );
+
+%%  MAKE POST - PRE
+
+proanti = kept2.keep_within_freqs( [0, 100] );
+% proanti = per_epoch.keep_within_freqs( [0, 100] );
+proanti = proanti.collapse( {'blocks', 'sessions'} );
+proanti = dsp2.process.manipulations.post_minus_pre( proanti );
+proanti = dsp2.process.manipulations.pro_v_anti( proanti );
+
+%%  PLOT
+% meaned = proanti.only( 'saline' );
+meaned = proanti;
+
+scale_name = 'rescaled';
+
+base_fname = dsp2.util.general.append_uniques( meaned, 'rescaled', {'epochs', 'drugs'} );
+
+pl = ContainerPlotter();
+pl.compare_series = false;
+pl.marker_size = 2;
+pl.add_ribbon = true;
+pl.main_line_width = 1;
+pl.x = meaned.frequencies;
+pl.shape = [2, 2];
+pl.y_lim = [-.02 .02];
+pl.y_label = 'Granger difference';
+pl.x_label = 'hz';
+pl.order_by = { 'real', 'permuted' };
+
+figure(1); clf();
+
+meaned.plot( pl, {'permuted', 'trialtypes', 'max_lags'}, {'regions', 'outcomes', 'drugs'} );
+
+save_path = fullfile( conf.PATHS.plots, dsp2.process.format.get_date_dir(), 'granger' );
+dsp2.util.general.require_dir( save_path );
+fname = fullfile( save_path, base_fname );
+dsp2.util.general.save_fig( figure(1), fname, {'fig', 'png', 'epsc'} );
 
 %%  PLOT PER DAY AND SAVE
 
