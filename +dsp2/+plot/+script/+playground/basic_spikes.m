3015ee53a0a8cfbea8d84f71b828034e7145ac62
nfagan <nick@fagan.org>
2017-09-19 15:23:15 -0400

    match dimensions

--- +dsp2/+plot/+script/+playground/basic_spikes.m
+++ +dsp2/+plot/+script/+playground/basic_spikes.m
@@ -1,19 +1,50 @@
 conf = dsp2.config.load();
 
-epoch = 'targacq';
-date_dir = datestr( now, 'mmddyy' );
-kind = 'pro_minus_anti';
-save_path = fullfile( conf.PATHS.plots, date_dir, 'mua_psth_raw', kind );
+epoch = 'reward';
+date_dir = dsp2.process.format.get_date_dir();
+kind = 'pro_v_anti_drug';
+save_path = fullfile( conf.PATHS.plots, 'mua', 'psth', date_dir, kind );
 do_normalize = false;
 bin_size = 25;
-% ylims = [-12.5, 12.5];
-ylims = [45, 90];
 
-load_path = fullfile( conf.PATHS.analyses, '081617', 'spikes' );
+do_save = true;
 
-%   load
-spikes = dsp2.util.general.fload( fullfile(load_path, [epoch, '.mat']) );
+% load_path = fullfile( conf.PATHS.analyses, '081617', 'spikes' );
+% %   load
+% spikes = dsp2.util.general.fload( fullfile(load_path, [epoch, '.mat']) );
 
+%
+%   begin new
+%
+load_path = fullfile( conf.PATHS.analyses, 'spikes', epoch );
+spikes = dsp2.util.general.load_mats( load_path, true );
+m_within1 = conf.SIGNALS.meaned.mean_within;
+to_clpse = { 'blocks', 'sessions' };
+m_within2 = setdiff( m_within1, to_clpse );
+m_within2 = union( m_within2, {'drugs', 'administration'} );
+parfor i = 1:numel(spikes)
+  disp( i );
+  current = spikes{i};
+  current = dsp2.process.spike.get_sps( current, bin_size );
+  current = current.each1d( m_within1, @rowops.mean );
+  current = dsp2.process.format.fix_block_number( current );
+  current = dsp2.process.format.fix_administration( current, 'config', conf );
+  if ( isempty(strfind(kind, 'drug')) )
+    current = dsp2.process.manipulations.non_drug_effect( current );
+    current = current.collapse( 'drugs' );
+  else
+    current = current.rm( 'unspecified' );
+  end
+  if ( ~isempty(current) )
+    current = current.each1d( m_within2, @rowops.mean );
+    current = current.collapse( to_clpse );
+  end
+  spikes{i} = current;
+end
+spikes = SignalContainer.concat( spikes );
+%
+%   end new
+%
 if ( do_normalize )
   baseline = dsp2.util.general.fload( fullfile(load_path, 'magcue.mat') );
   baseline = baseline.mean(2);
@@ -22,10 +53,22 @@ if ( do_normalize )
   end
 end
 
+%%
 plt = spikes;
 plt = plt.rm( {'ref', 'errors'} );
 
 if ( any(plt.contains({'targacq', 'targAcq'})) ), plt = plt.rm( 'cued' ); end
+if ( any(plt.contains({'targon', 'targOn'})) ), plt = plt.rm( 'choice' ); end
+
+if ( ~isempty(strfind(kind, 'drug')) )
+  plt = dsp2.process.manipulations.post_minus_pre( plt );
+end
+if ( ~isempty(strfind(kind, 'pro')) )
+  plt = dsp2.process.manipulations.pro_v_anti( plt );
+end
+if ( ~isempty(strfind(kind, 'minus_anti')) )
+  plt = dsp2.process.manipulations.pro_minus_anti( plt );
+end
 
 plts = plt.enumerate( {'trialtypes'} );
 
@@ -34,22 +77,16 @@ dsp2.util.general.require_dir( save_path );
 for k = 1:numel(plts)
   
   plt = plts{k};
-
-  for i = 1:size(plt.data, 1)
-    plt.data(i, :) = smooth( plt.data(i, :) );
+  dat = plt.data;
+  for i = 1:size(dat, 1)
+    dat(i, :) = smooth( dat(i, :) );
   end
+  plt.data = dat;
 
-  if ( ~isempty(strfind(kind, 'pro')) )
-    plt = dsp2.process.manipulations.pro_v_anti( plt );
-  end
-  if ( ~isempty(strfind(kind, 'minus_anti')) )
-    plt = dsp2.process.manipulations.pro_minus_anti( plt );
-  end
+  scale_factor = plt.fs / 1e3;
 
-  scale_factor = spikes.fs / 1e3;
-
-  start = spikes.start;
-  stop = spikes.stop + spikes.window_size;
+  start = plt.start;
+  stop = plt.stop + plt.window_size;
   amt = stop - start;
 
   figure(1); clf();
@@ -57,20 +94,30 @@ for k = 1:numel(plts)
   pl = ContainerPlotter();
 
   pl.x = start:bin_size:start+amt-1;
-  pl.x_label = sprintf( 'Time (ms) from %s', strjoin(plt('epochs'), ', ') );
+%   pl.x_label = sprintf( 'Time (ms) from %s', strjoin(plt('epochs'), ', ') );
   pl.y_label = 'sp/s';
   pl.y_lim = ylims;
+  pl.y_lim = [-50, 50];
+  pl.shape = [2, 2];
   pl.add_ribbon = true;
+  pl.vertical_lines_at = 0;
+  pl.main_line_width = 1;
 
   plt = plt.require_fields( 'proanti' );
   plt( 'proanti', plt.where({'self', 'none'}) ) = 'anti';
   plt( 'proanti', plt.where({'both', 'other'}) ) = 'pro';
 
-  plt.plot( pl, 'outcomes', {'regions', 'trialtypes'} );
+  plt.plot( pl, {'outcomes'}, {'trialtypes', 'administration', 'drugs', 'regions'} );
 
-  fname = dsp2.util.general.append_uniques( plt, 'mua', {'epochs', 'trialtypes'} );
+  fname = dsp2.util.general.append_uniques( plt, 'mua' ...
+    , {'epochs', 'trialtypes', 'drugs', 'administration'} );
   fname = fullfile( save_path, fname );
+  
+  f = FigureEdits( gcf );
+  f.one_legend();
 
-%   dsp2.util.general.save_fig( gcf, fname, {'eps', 'png', 'fig'} );
+  if ( do_save )
+    dsp2.util.general.save_fig( gcf, fname, {'eps', 'png', 'fig'} );
+  end
 
 end
