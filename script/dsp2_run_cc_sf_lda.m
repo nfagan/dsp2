06cce3b40da00a8f2539ed25e08b087746d624e6
nfagan <nick@fagan.org>
2018-12-06 12:11:13 -0500

    change parpool setup

--- script/dsp2_run_cc_sf_lda.m
+++ script/dsp2_run_cc_sf_lda.m
@@ -6,10 +6,13 @@ end
 
 repadd( 'dsp3/script' );
 
-dsp2.cluster.init();
+dsp2.util.general.add_depends();
+% p = parpool();
 
+fprintf( '\n Loading ...' );
 data = shared_utils.io.fload( fullfile(data_p, 'cc_sf_coh_data.mat') );
 labels = shared_utils.io.fload( fullfile(data_p, 'cc_sf_coh_labels.mat') );
+fprintf( ' Done.' );
 
 labels = fcat.from( labels );
 
